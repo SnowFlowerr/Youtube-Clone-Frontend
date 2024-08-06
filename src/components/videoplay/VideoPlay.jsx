b0038e8ff62963cc3ec6e53acf57bec4ce89cdfa
SnowFlowerr <bhudeokrit@gmail.com>
2024-08-06 20:20:30 +0530

    functionalities added

--- src/components/videoplay/VideoPlay.jsx
+++ src/components/videoplay/VideoPlay.jsx
@@ -28,11 +28,17 @@ export default function VideoPlay() {
         async function fetchData() {
             try {
                 const vidData = await axios.get(`http://localhost:8000/api/videos/${id}`)
-                const userD= await axios.get(`http://localhost:8000/api/users/${vidData.data.userId}`)
+                const userD = await axios.get(`http://localhost:8000/api/users/${vidData.data.userId}`)
                 setvideoData(vidData.data)
                 setuserData(userD.data)
-                if(userD.data.followedUser.indexOf('669b3f0247fef352b02613c6')!==-1){
+                if (userD.data.followedUser.indexOf('669b3f0247fef352b02613c6') !== -1) {
                     setisSubs(true)
+                }
+                if (vidData.data.likedUser.indexOf('669b3f0247fef352b02613c6') !== -1) {
+                    setisLike(true)
+                }
+                if (vidData.data.dislikedUser.indexOf('669b3f0247fef352b02613c6') !== -1) {
+                    setisDislike(true)
                 }
             }
             catch (err) {
@@ -44,9 +50,9 @@ export default function VideoPlay() {
     useEffect(() => {
         async function fetchData() {
             try {
-                const userD= await axios.get(`http://localhost:8000/api/users/${videoData.userId}`)
+                const userD = await axios.get(`http://localhost:8000/api/users/${videoData.userId}`)
                 setuserData(userD.data)
-                if(userD.data.followedUser.indexOf('669b3f0247fef352b02613c6')!==-1){
+                if (userD.data.followedUser.indexOf('669b3f0247fef352b02613c6') !== -1) {
                     setisSubs(true)
                 }
             }
@@ -56,6 +62,24 @@ export default function VideoPlay() {
         }
         fetchData()
     }, [isSubs])
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const vidData = await axios.get(`http://localhost:8000/api/videos/${id}`)
+                setvideoData(vidData.data)
+                if (vidData.data.likedUser.indexOf('669b3f0247fef352b02613c6') !== -1) {
+                    setisLike(true)
+                }
+                if (vidData.data.dislikedUser.indexOf('669b3f0247fef352b02613c6') !== -1) {
+                    setisDislike(true)
+                }
+            }
+            catch (err) {
+                console.log(err)
+            }
+        }
+        fetchData()
+    }, [isLike, isDislike])
 
     useEffect(() => {
         if (menu) {
@@ -68,14 +92,14 @@ export default function VideoPlay() {
     function handleSubscribe() {
         async function fetchData() {
             try {
-                if(isSubs){
+                if (isSubs) {
                     await axios.put(`http://localhost:8000/api/users/unsubscribe/${videoData.userId}`,
                         {},
                         { withCredentials: true }
                     );
                     console.log("Unsubscribe")
                 }
-                else{
+                else {
                     await axios.put(`http://localhost:8000/api/users/subscribe/${videoData.userId}`,
                         {},
                         { withCredentials: true }
@@ -91,17 +115,67 @@ export default function VideoPlay() {
         }
         fetchData()
     }
-    function handleLike() {
-        setisLike(!isLike)
+    async function handleLike() {
+        if (isDislike === true) {
+            handleDislike()
+        }
+        // else if(isLike===false && isDislike===true){
+        //     handleLike()
+        // }
+        try {
+            if (isLike) {
+                await axios.put(`http://localhost:8000/api/videos/unlike/${id}`,
+                    {},
+                    { withCredentials: true }
+                );
+                console.log("unlikes")
+            }
+            else {
+                await axios.put(`http://localhost:8000/api/videos/like/${id}`,
+                    {},
+                    { withCredentials: true }
+                );
+                console.log("likes")
+            }
+            setisLike(!isLike)
+            // console.log("err?.response?.data")
+        }
+        catch (err) {
+            console.log(err?.response?.data)
+        }
     }
-    function handleDislike() {
+    async function handleDislike() {
+        if (isLike === true) {
+            handleLike()
+        }
+        try {
+            if (isDislike) {
+                await axios.put(`http://localhost:8000/api/videos/undislike/${id}`,
+                    {},
+                    { withCredentials: true }
+                );
+                console.log("undislikes")
+            }
+            else {
+                await axios.put(`http://localhost:8000/api/videos/dislike/${id}`,
+                    {},
+                    { withCredentials: true }
+                );
+                console.log("dislikes")
+            }
+            setisDislike(!isDislike)
+            // console.log("err?.response?.data")
+        }
+        catch (err) {
+            console.log(err?.response?.data)
+        }
         setisDislike(!isDislike)
     }
-    async function play(){
-        try{
+    async function play() {
+        try {
             await axios.put(`http://localhost:8000/api/videos/view/${id}`)
         }
-        catch(err){
+        catch (err) {
             console.log(err)
         }
     }
@@ -129,7 +203,7 @@ export default function VideoPlay() {
                         ref={videoRef}
                         onPlay={play}
                         poster={null}>
-                            
+
 
                         <source
                             src={vid}
@@ -144,46 +218,48 @@ export default function VideoPlay() {
             <div className={styles.videoDetails} ref={boxRef}>
                 <div className={styles.videoDet}>
                     <div className={styles.title}>
-                        {videoData?.title} {videoData?.userId}
+                        {videoData?.title}
                     </div>
 
                     <div className={styles.videoStatus}>
                         <div className={styles.channel}>
+                            <div className={styles.channelDetail}>
                             <div className={styles.icon}></div>
                             <div className={styles.channelName}>
                                 <span className={styles.name}>{userData?.name}</span>
                                 <br />
                                 <span className={styles.subs}>{userData?.followers} subs</span>
                             </div>
-                            {/* <div className={styles.subscribe}>
-                                Subscribe
-                            </div> */}
-                        </div>
-                        <div className={styles.status2}>
-                            <div className={styles.subscribe} style={theme ? isSubs?{backgroundColor:"#2e2e2e"}:lightTheme : isSubs?{backgroundColor:"rgb(220, 220, 220)"}:darkTheme} onClick={handleSubscribe}>
-                                {isSubs ? "Unsubscribe" : "Subscribe"}
                             </div>
-                            <div className={styles.status}>
-                                <div className={styles.likeDislike} style={theme ? {} : { backgroundColor: "rgb(220, 220, 220)" }}>
-                                    <span className={styles.like} onClick={handleLike} >
-                                        {isLike ? <i className="fa-solid fa-thumbs-up"></i> : <i className="fa-regular fa-thumbs-up"></i>} {videoData?.likes}
-                                    </span>
-                                    <span className={styles.dislike} onClick={handleDislike}>
-                                        {isDislike ? <i className="fa-solid fa-thumbs-down fa-flip-horizontal"></i> : <i className="fa-regular fa-thumbs-down fa-flip-horizontal"></i>} {videoData?.dislikes}
-                                    </span>
+
+                            <div className={styles.status2}>
+                                <div className={styles.subscribe} style={theme ? isSubs ? { backgroundColor: "#2e2e2e" } : lightTheme : isSubs ? { backgroundColor: "rgb(220, 220, 220)" } : darkTheme} onClick={handleSubscribe}>
+                                    {isSubs ? "Unsubscribe" : "Subscribe"}
                                 </div>
-                                <Share
+
+                            </div>
+                        </div>
+                        <div className={styles.status}>
+                            <div className={styles.likeDislike} style={theme ? {} : { backgroundColor: "rgb(220, 220, 220)" }}>
+                                <span className={styles.like} onClick={handleLike} >
+                                    {isLike ? <i className="fa-solid fa-thumbs-up"></i> : <i className="fa-regular fa-thumbs-up"></i>} {videoData?.likes}
+                                </span>
+                                <span className={styles.dislike} onClick={handleDislike}>
+                                    {isDislike ? <i className="fa-solid fa-thumbs-down fa-flip-horizontal"></i> : <i className="fa-regular fa-thumbs-down fa-flip-horizontal"></i>} {videoData?.dislikes}
+                                </span>
+                            </div>
+                            <Share
                                 text={videoData?.title}
                                 url={window.location.href}
                                 title={videoData?.title}
-                                    share={
-                                        <div className={styles.share} style={theme ? {} : { backgroundColor: "rgb(220, 220, 220)" }}>
-                                            <span><i className="fa-solid fa-share"></i> Share</span>
-                                        </div>
-                                    }
-                                />
-                            </div>
+                                share={
+                                    <div className={styles.share} style={theme ? {} : { backgroundColor: "rgb(220, 220, 220)" }}>
+                                        <span><i className="fa-solid fa-share"></i> Share</span>
+                                    </div>
+                                }
+                            />
                         </div>
+
                     </div>
                     <div className={styles.descript} style={theme ? {} : { backgroundColor: "rgb(220, 220, 220)" }}>
                         <b>{videoData?.views} views</b> <br />
@@ -210,6 +286,33 @@ export default function VideoPlay() {
 
                     fefsdz <br />
                     efvfesv
+                    v
+                    fefsdz <br />
+                    efvfesv
+                    v
+                    fefsdz <br />
+                    efvfesv
+                    fefsdz <br />
+                    efvfesv
+
+                    fefsdz <br />
+                    efvfesv
+                    fefsdz <br />
+                    efvfesv
+                    fefsdz <br />
+                    efvfesv
+                    v
+                    fefsdz <br />
+                    efvfesv
+                    v
+                    fefsdz <br />
+                    efvfesv
+                    fefsdz <br />
+                    efvfesv
+
+                    fefsdz <br />
+                    efvfesv
+                    fefsdz <br />
                     v
                     fefsdz <br />
                     efvfesv
