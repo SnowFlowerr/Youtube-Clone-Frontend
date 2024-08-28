d6c40a75e58e1e7ab5228b8d3d3f75f95cd828a0
SnowFlowerr <bhudeokrit@gmail.com>
2024-08-28 21:49:14 +0530

    search history improved

--- src/components/auth/firebase.js
+++ src/components/auth/firebase.js
@@ -7,12 +7,12 @@ import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
+    apiKey: process.env.REACT_APP_API_KEYS,
+    authDomain: process.env.REACT_APP_AUTH_DOMAINS,
+    projectId: process.env.REACT_APP_PROJECT_IDS,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKETS,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_IDS,
+    appId: process.env.REACT_APP_APP_IDS,
 };
 
 // Initialize Firebase
