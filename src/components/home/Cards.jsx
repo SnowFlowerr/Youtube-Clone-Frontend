2d80ed77e0a7367e713f3aa90084ab2ba786c2cf
SnowFlowerr <bhudeokrit@gmail.com>
2024-08-31 18:20:33 +0530

    hover timeline improved

--- src/components/home/Cards.jsx
+++ src/components/home/Cards.jsx
@@ -37,7 +37,7 @@ export default function Cards({ video }) {
     useEffect(() => {
         async function currentUser() {
             try {
-                const userD = await axios.get(`http://localhost:8000/api/users/get/${video?.userId}`,
+                const userD = await axios.get(`https://honest-stillness-production.up.railway.app/api/users/get/${video?.userId}`,
                     { withCredentials: true }
                 )
                 setUser(userD.data)
